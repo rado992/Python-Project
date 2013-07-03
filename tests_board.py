684b56ab620059e92ecc7dc7b3e5a1a40ec53d85
Radoslav Varbanov <radvarbanov@gmail.com>
2013-07-04 01:42:51 +0300

    Write tests for board

---
+++ tests_board.py
@@ -0,0 +1,48 @@
+import unittest
+from board import Board
+
+
+CLASSIC_GAME = [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5,
+                0, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, 0, 0]
+
+
+class BoardTest(unittest.TestCase):
+    def test_new_board(self):
+        board = Board()
+        field = {}
+        field['W'] = CLASSIC_GAME
+        field['B'] = CLASSIC_GAME
+        self.assertEqual(field, board.get_board())
+
+    def test_move_tile(self):
+        board = Board()
+        board.move_checker('W', 1, 3)
+        self.assertEqual(1, board.get_board()['W'][4])
+        self.assertEqual(1, board.get_board()['W'][1])
+        board.move_checker('W', 1, 3)
+        self.assertEqual(2, board.get_board()['W'][4])
+        self.assertEqual(0, board.get_board()['W'][1])
+
+    def test_forbidden_move(self):
+        board = Board()
+        self.assertEqual(False, board.move_checker('W', 1, 5))
+
+    def test_knock_tile(self):
+        board = Board()
+        board.move_checker('W', 1, 6)
+        board.move_checker('B', 17, 1)
+        #print(board.get_board())
+        self.assertEqual(1, board.get_board()['W'][0])
+        self.assertEqual(1, board.get_board()['B'][18])
+
+    def test_dice_throw(self):
+        board = Board()
+        for i in range(50):
+            for j in board.throw_dice():
+                self.assertIn(j, range(7))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
